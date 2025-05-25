5f55c4b96e0c2fe4689d4abd0e349e7c9a512cde
electrosharma123 <97042858+electrosharma123@users.noreply.github.com>
2025-05-25 11:41:24 +0000

    jsonifyed project

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, json, render_template, jsonify
 
 app = Flask(__name__)
+
+partners = [
+    {
+        "partnerName": "BloggingSharma",
+        "partnerLink": "https://bloggingsharma.com",
+    },
+    {
+        "partnerName": "AgentSharma",
+        "partnerLink": "https://Agentsharma.com",
+    },
+    {
+        "partnerName": "MultiSharma",
+        "partnerLink": "https://Multisharma.com",
+    },
+    {
+        "partnerName": "HackerSharma",
+        "partnerLink": "https://Hackersharma.com",
+    },
+]
+
+print(partners)
 
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",
+                           webName="BloggingSharma",
+                           partner=partners)
+
+
+@app.route("/partners")
+def partner():
+    return jsonify(partners)
 
 
 if __name__ == "__main__":
